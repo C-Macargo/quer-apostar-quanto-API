f581868757a2cf58bb6300ef25a132492706705f
C-Macargo <caio.camargos97@gmail.com>
2024-01-28 20:20:16 -0300

    feat: adding joi validation

--- src/router/participantRouter.ts
+++ src/router/participantRouter.ts
@@ -1,9 +1,11 @@
 import { createParticipant, findParticipants } from "@/controller/participantController";
+import { validateSchema } from "@/middleware/validateSchemaMiddleware";
+import { participantSchema } from "@/schema/participantSchema";
 import { Router } from "express";
 
 const participantRouter = Router()
 
-participantRouter.get('/', findParticipants)
-participantRouter.post('/', createParticipant)
+participantRouter.get('/',  findParticipants)
+participantRouter.post('/',validateSchema(participantSchema), createParticipant)
 
 export default participantRouter
---
+++ src/schema/participantSchema.ts
@@ -0,0 +1,6 @@
+import Joi, { Schema } from 'joi';
+
+export const participantSchema: Schema = Joi.object({
+    name: Joi.string().required(),
+    balance: Joi.number().strict().min(10000).required(),
+});
