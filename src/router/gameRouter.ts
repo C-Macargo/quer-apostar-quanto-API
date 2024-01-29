cb0d390fe97d0cb407d6ccef19543f0efe0db755
C-Macargo <caio.camargos97@gmail.com>
2024-01-29 12:00:43 -0300

    feat: add joi validation to create game

--- src/router/gameRouter.ts
+++ src/router/gameRouter.ts
@@ -1,9 +1,10 @@
 import { createGame } from "@/controller/gameController";
 import { validateSchema } from "@/middleware/validateSchemaMiddleware";
+import { gameSchema } from "@/schema/gameSchema";
 import { Router } from "express";
 
 const gameRouter = Router()
 
-gameRouter.post('/',  createGame)
+gameRouter.post('/',validateSchema(gameSchema),  createGame)
 
 export default gameRouter
---
+++ src/schema/gameSchema.ts
@@ -0,0 +1,6 @@
+import Joi, { Schema } from 'joi';
+
+export const gameSchema: Schema = Joi.object({
+    homeTeamName: Joi.string().required(),
+    awayTeamName: Joi.string().required(),
+});
